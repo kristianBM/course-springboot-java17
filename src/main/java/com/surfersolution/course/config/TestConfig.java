cc2250fab79242181669a26881dd2a779d431ce9
Kristian Mello <kristian.bonfim@hotmail.com>
2021-11-05 00:42:56 -0300

    Payment, one-to-one association

--- src/main/java/com/surfersolution/course/config/TestConfig.java
+++ src/main/java/com/surfersolution/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.surfersolution.course.entities.Category;
 import com.surfersolution.course.entities.Order;
 import com.surfersolution.course.entities.OrderItem;
+import com.surfersolution.course.entities.Payment;
 import com.surfersolution.course.entities.Product;
 import com.surfersolution.course.entities.User;
 import com.surfersolution.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 	
 	
