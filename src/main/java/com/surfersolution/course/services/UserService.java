4862875e25a9c034ec7ffd271201141e52bf6391
Kristian Mello <kristian.bonfim@hotmail.com>
2021-11-05 11:04:16 -0300

    user update excpetion

--- src/main/java/com/surfersolution/course/services/UserService.java
+++ src/main/java/com/surfersolution/course/services/UserService.java
@@ -3,12 +3,16 @@ package com.surfersolution.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.surfersolution.course.entities.User;
 import com.surfersolution.course.repositories.UserRepository;
+import com.surfersolution.course.services.exceptions.DatabaseException;
 import com.surfersolution.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -35,15 +39,20 @@ public class UserService {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
 		}
 		
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
-		
+		try {
+			User entity = repository.getById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 		
 	}
 
